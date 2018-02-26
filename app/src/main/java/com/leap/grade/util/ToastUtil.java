a0c66f8b44a72b839cd013ceaf0d58bae5d6628b
weiyaling <weiyaling@hd123.com>
2018-02-26 10:17:33 +0800

    ready

---
+++ app/src/main/java/com/leap/grade/util/ToastUtil.java
@@ -0,0 +1,9 @@
+package com.leap.grade.util;
+
+/**
+ * ToastUtil : Toast工具类
+ * <p>
+ * </> Created by ylwei on 2018/2/24.
+ */
+public class ToastUtil {
+}
---
+++ app/src/main/java/com/leap/grade/widget/ClearEditText.java
@@ -0,0 +1,10 @@
+package com.leap.grade.widget;
+
+/**
+ * ClearEditText : 统一输入框控件
+ * <p>
+ * </> Created by ylwei on 2018/2/24.
+ */
+public class ClearEditText {
+
+}
